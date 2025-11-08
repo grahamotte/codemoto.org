ff6928bbcc5219b61d56b3e849dd387f37cd037f
Graham Otte <go@graham.lol>
2025-11-07 22:40:19 -0800

    WIP Fri Nov  7 22:40:19 PST 2025

--- backend/app/controllers/api/noop_controller.rb
+++ backend/app/controllers/api/noop_controller.rb
@@ -14,7 +14,7 @@ module Api
       render json: {
         frontend_time: params[:frontend_time],
         backend_time: Time.current,
-        database_time: ActiveRecord::Base.connection.execute("SELECT NOW()")[0]["now"].utc
+        database_time: ActiveRecord::Base.connection.execute("SELECT NOW()")[0]["now"].utc,
       }
     end
   end
