94b5b5d88983fe54c24b28a4ed881f7812fd8bff
Graham Otte <go@graham.lol>
2026-02-20 21:15:00 -0800

    Update HcJob schedule test

--- backend/test/jobs/hc_job_test.rb
+++ backend/test/jobs/hc_job_test.rb
@@ -2,6 +2,6 @@ require "test_helper"
 
 class HcJobTest < ActiveSupport::TestCase
   def test_schedule_interval
-    assert_equal "every 1 minute", HcJob.schedule_interval
+    assert_equal "every 1 hour", HcJob.schedule_interval
   end
 end
