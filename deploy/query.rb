314e9e2bc304ff7e32955f9c33ccadea0c20b977
Graham Otte <go@graham.lol>
2026-05-06 21:04:07 -0700

    Add query task

---
+++ deploy/query.rb
@@ -0,0 +1,29 @@
+require_relative "lib/require"
+
+query = ARGV.join(" ").strip
+
+if query.blank?
+  warn "Usage: mise deploy:query \"select count(*) from table_name\""
+  exit 1
+end
+
+unless /\A\s*(select|with|show|explain|table|values)\b/i.match?(query)
+  warn "Only read-only query forms are allowed: SELECT, WITH, SHOW, EXPLAIN, TABLE, VALUES"
+  exit 1
+end
+
+Cmd.ssh(
+  [
+    "PGOPTIONS='-c default_transaction_read_only=on'",
+    "psql",
+    "-X",
+    "-v ON_ERROR_STOP=1",
+    "-P pager=off",
+    "-U ?",
+    "-d ?",
+    "-c ?",
+  ].join(" "),
+  Constants.deploy_user,
+  Constants.db_name,
+  query,
+)
