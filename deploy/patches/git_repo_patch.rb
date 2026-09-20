3568eb729a7eef29ca9dbf32fee43e690d332327
Graham Otte <go@graham.lol>
2026-09-19 21:24:21 -0700

    Push GitHub as origin instead of a separate github remote.

--- deploy/patches/git_repo_patch.rb
+++ deploy/patches/git_repo_patch.rb
@@ -1,7 +1,7 @@
 class GitRepoPatch < BasePatch
   class << self
     def always
-      push_to("github", Constants.github_repo)
+      push_to("origin", Constants.github_repo)
     end
 
     private
